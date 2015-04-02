cc09a1dc23068ee8bf5f2c86f7373ddba6688fa6
Sterling Smart <sterling.smart@infospace.com>
2015-04-02 16:09:20 -0700

    Add stub for AWS Kinesis log4net appender

---
+++ src/log4net.AwsKinesisAppender/AwsKinesisAppender.cs
@@ -0,0 +1,13 @@
+﻿using log4net.Appender;
+using log4net.Core;
+
+namespace log4net.Ext.Appender
+{
+    public class AwsKinesisAppender : AppenderSkeleton
+    {
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            // TODO: forward event to AWS Kinesis
+        }
+    }
+}
