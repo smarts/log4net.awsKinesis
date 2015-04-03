782323a7c6b1b4f159e970fdd9b2445f13d12f3b
Sterling Smart <sterling.smart@infospace.com>
2015-04-02 21:15:31 -0700

    Add interface and default implementation for AWS Kinesis client factory
    Working within the log4net conventions does not allow for injecting
    appender dependencies (e.g., the AWS Kinesis client) via the
    constructor. Instead we can inject the client via a property. However,
    since the convention of the AWS SDK is to use a static method on the
    AWSClientFactory class to create a client instance instead of directly
    instantiating the client instance, we will create a property on the
    appender for the client factory instead of the client.

---
+++ src/log4net.AwsKinesisAppender/AwsKinesisFactory.cs
@@ -0,0 +1,13 @@
+﻿using Amazon;
+using Amazon.Kinesis;
+
+namespace log4net.Ext.Appender.AwsKinesis
+{
+    internal class AwsKinesisFactory : IAwsKinesisFactory
+    {
+        public IAmazonKinesis Create()
+        {
+            return AWSClientFactory.CreateAmazonKinesisClient();
+        }
+    }
+}
---
+++ src/log4net.AwsKinesisAppender/IAwsKinesisFactory.cs
@@ -0,0 +1,9 @@
+﻿using Amazon.Kinesis;
+
+namespace log4net.Ext.Appender.AwsKinesis
+{
+    public interface IAwsKinesisFactory
+    {
+        IAmazonKinesis Create();
+    }
+}
